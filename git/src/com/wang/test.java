72da8134e97cffa5ee286c074152e53d1ea260a1
Afflatus <67040612+18222137497@users.noreply.github.com>
2020-09-03 00:43:03 +0800

    测试修改

--- git/src/com/wang/test.java
+++ git/src/com/wang/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("第一次修改");
+        System.out.println("测试修改");
     }
 }
